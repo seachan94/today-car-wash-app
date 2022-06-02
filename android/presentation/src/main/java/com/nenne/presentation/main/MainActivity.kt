346d0aebb1197c75eaf6d2e214a6c167b9d6d570
choesechan <seachan94@naver.com>
2022-06-02 16:10:40 +0900

    NEN-18 feat : kakao map sdk 추가

--- android/presentation/src/main/java/com/nenne/presentation/main/MainActivity.kt
+++ android/presentation/src/main/java/com/nenne/presentation/main/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.nenne.presentation.main
 
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.content.pm.Signature
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import com.nenne.presentation.base.BaseActivity
 import com.nocompany.presentation.R
 import com.nocompany.presentation.databinding.ActivityMainBinding
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +23,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        getHashKey()
     }
+    fun getHashKey(){
+        var packageInfo : PackageInfo = PackageInfo()
+        try {
+            packageInfo = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+        } catch (e: PackageManager.NameNotFoundException){
+            e.printStackTrace()
+        }
 
+        for (signature: Signature in packageInfo.signatures){
+            try{
+                var md: MessageDigest = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                Log.e("KEY_HASH", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+            } catch(e: NoSuchAlgorithmException){
+                Log.e("KEY_HASH", "Unable to get MessageDigest. signature = " + signature, e)
+            }
+        }
+    }
 }
--- android/presentation/src/main/java/com/nenne/presentation/map/MapFragment.kt
+++ android/presentation/src/main/java/com/nenne/presentation/map/MapFragment.kt
@@ -1,14 +1,55 @@
 package com.nenne.presentation.map
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.nenne.presentation.base.BaseFragment
 import com.nocompany.presentation.R
 import com.nocompany.presentation.databinding.FragmentMapBinding
+import net.daum.mf.map.api.MapView
 
 class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map) {
+
+    override fun initViewStatus() = with(binding){
+        val mapView = MapView(requireActivity())
+        clKakaoMapView.addView(mapView)
+
+        val deniedPermissions = getDeniedPermissions()
+        requestPermissions(deniedPermissions)
+
+    }
+
+    private val multiplePermissionsCode = 100
+    private val requiredPermissions = arrayOf(
+        Manifest.permission.INTERNET,
+        Manifest.permission.USE_FULL_SCREEN_INTENT,
+        Manifest.permission.FOREGROUND_SERVICE,
+        Manifest.permission.RECEIVE_BOOT_COMPLETED
+    )
+    //test
+    private fun getDeniedPermissions():ArrayList<String>{
+        var deniedPermissions = ArrayList<String>()
+
+        for(p in requiredPermissions){
+            if(ContextCompat.checkSelfPermission(requireContext(), p) == PackageManager.PERMISSION_DENIED){
+                deniedPermissions.add(p)
+            }
+        }
+
+        return deniedPermissions
+    }
+
+    private fun requestPermissions(permissions: ArrayList<String>){
+        if(permissions.isNotEmpty()){
+            val array = arrayOfNulls<String>(permissions.size)
+            ActivityCompat.requestPermissions(requireActivity(), permissions.toArray(array), 1)
+        }
+    }
 
 }
