48ba07dab9f61136b0cfd458041a1473754f8b61
choesechan <seachan94@naver.com>
2022-06-08 22:09:55 +0900

    NEN-18 feat : 키 오류 해결

--- android/presentation/src/main/java/com/nenne/presentation/main/MainActivity.kt
+++ android/presentation/src/main/java/com/nenne/presentation/main/MainActivity.kt
@@ -9,6 +9,7 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import com.nenne.presentation.base.BaseActivity
+import com.nocompany.presentation.BuildConfig
 import com.nocompany.presentation.R
 import com.nocompany.presentation.databinding.ActivityMainBinding
 import java.security.MessageDigest
@@ -18,28 +19,11 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
 
+    val TAG = "sechan"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        getHashKey()
     }
-    fun getHashKey(){
-        var packageInfo : PackageInfo = PackageInfo()
-        try {
-            packageInfo = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
-        } catch (e: PackageManager.NameNotFoundException){
-            e.printStackTrace()
-        }
 
-        for (signature: Signature in packageInfo.signatures){
-            try{
-                var md: MessageDigest = MessageDigest.getInstance("SHA")
-                md.update(signature.toByteArray())
-                Log.e("KEY_HASH", Base64.encodeToString(md.digest(), Base64.DEFAULT))
-            } catch(e: NoSuchAlgorithmException){
-                Log.e("KEY_HASH", "Unable to get MessageDigest. signature = " + signature, e)
-            }
-        }
-    }
 }
